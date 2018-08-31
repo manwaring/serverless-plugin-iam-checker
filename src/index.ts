caab7540e60a49e0c3f646116ef95d6800a61499
Phillip Manwaring <phillip.manwaring@gmail.com>
2018-08-30 21:26:16 -0400

    add logic checks

--- src/index.ts
+++ src/index.ts
@@ -19,20 +19,38 @@ class IamChecker {
     // };
 
     this.hooks = {
-      "after:deploy:createDeploymentArtifacts": this.beforeWelcome.bind(this)
+      'after:deploy:createDeploymentArtifacts': this.checkIam.bind(this)
     };
   }
 
   serverless: any;
   hooks: any;
 
-  beforeWelcome() {
-    this.serverless.cli.log(
-      JSON.stringify(
-        this.serverless.service.provider.compiledCloudFormationTemplate
-      )
-    );
-    this.serverless.cli.log("Hello from Serverless!");
+  checkIam() {
+    const resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+    const roles = resources.filter(resource => resource.Type === 'AWS::IAM::Role');
+    roles.forEach(role => this.checkRole(role));
+    this.serverless.cli.log('Hello from Serverless!');
+  }
+
+  checkRole(role: any) {
+    const policies = role.Properties.Policies;
+    policies.forEach(policy => this.checkPolicy(policy));
+  }
+
+  checkPolicy(policy: any) {
+    const statements = policy.Statement;
+    statements.forEach(statement => this.checkStatement(statement));
+  }
+
+  checkStatement(statement: any) {
+    const actions = statement.Action;
+    const resources = statement.Resource;
+    const starActions = actions.find(action => action && action.indexOf('*') > -1);
+    const starResources = resources.find(resource => resource && resource.indexOf('*') > -1);
+    if (starActions.length > 0 || starResources.length > 0) {
+      throw new this.serverless.classes.Error(`Actions or resources contain *`);
+    }
   }
 }
 
