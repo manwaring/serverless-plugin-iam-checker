c8a0c4c6b7fe89ce90e4b065474611f5d2a156b7
Phillip <phillip.manwaring@gmail.com>
2018-09-03 17:11:29 -0400

    add additional logging

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ class IamChecker {
     // };
 
     this.hooks = {
-      'after:deploy:createDeploymentArtifacts': this.checkIam.bind(this)
+      'after:package:createDeploymentArtifacts': this.checkIam.bind(this)
     };
   }
 
@@ -27,29 +27,40 @@ class IamChecker {
   hooks: any;
 
   checkIam() {
-    const resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+    let resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
     const roles = [];
+    const keys = Object.keys(resources);
+    this.log(JSON.stringify(resources));
+    this.log(keys);
+    this.log(JSON.stringify(keys));
     Object.keys(resources).forEach(key => {
+      this.log(key);
       if (resources[key].Type === 'AWS::IAM::Role') {
         roles.push(resources[key]);
       }
     });
     // const roles = resources.filter(resource => resource.Type === 'AWS::IAM::Role');
+    this.log(roles);
     roles.forEach(role => this.checkRole(role));
-    this.serverless.cli.log('Hello from Serverless!');
+    this.log('Hello from Serverless!');
   }
 
   checkRole(role: any) {
+    this.log(role);
     const policies = role.Properties.Policies;
+    this.log(policies);
     policies.forEach(policy => this.checkPolicy(policy));
   }
 
   checkPolicy(policy: any) {
+    this.log(policy);
     const statements = policy.Statement;
+    this.log(statements);
     statements.forEach(statement => this.checkStatement(statement));
   }
 
   checkStatement(statement: any) {
+    this.log(statement);
     const actions = statement.Action;
     const resources = statement.Resource;
     const starActions = actions.find(action => action && action.indexOf('*') > -1);
@@ -57,6 +68,10 @@ class IamChecker {
     if (starActions.length > 0 || starResources.length > 0) {
       throw new this.serverless.classes.Error(`Actions or resources contain *`);
     }
+  }
+
+  log(...args) {
+    this.serverless.cli.log(args);
   }
 }
 
